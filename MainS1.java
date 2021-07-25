ae9a360a8b2ea2f33a2dbebf868c7c591930fb26
Milaaver <redspot2015@mail.ru>
2021-07-25 19:12:18 +0700

    Java file

---
+++ MainS1.java
@@ -0,0 +1,8 @@
+public class MainS1 {
+    public static void main(String[] args) {
+        int ticketPrice = 10000;
+        int miles = ticketPrice / 20;
+
+        System.out.println(miles);
+    }
+}
